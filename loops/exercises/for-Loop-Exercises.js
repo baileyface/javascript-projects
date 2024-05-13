9b90f3d21f254d5fa2e38ea867fc67e7f2df6a8a
Bailey Johnson <bailey506@hotmail.com>
2024-05-13 16:17:33 -0500

    Loop Exercises

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -1,24 +1,71 @@
 /*Exercise #1: Construct for loops that accomplish the following tasks:
-    a. Print the numbers 0 - 20, one number per line.
-    b. Print only the ODD values from 3 - 29, one number per line.
-    c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
-    d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+    a. Print the numbers 0 - 20, one number per line.*/
+    for (let i = 0; i <= 20; i++) {
+      console.log(i);
+    }
 
+
+    /*b. Print only the ODD values from 3 - 29, one number per line.*/
+    for (let i = 3; i <= 29; i = i + 2) {
+      console.log(i);
+    }
+    
+   
+   
+   
+    /* c. Print the EVEN numbers 12 to -14 in descending order, one number per line.*/
+    for (let i = 12; i >= -14; i = i - 2) {
+      console.log(i);
+    }
+
+    
+    
+   /* d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+  let firstNumber = 50;
+  let lastNumber = 20;
+
+  for (let i = firstNumber; i >= lastNumber; i--) {
+    if (i % 3 === 0) {
+      console.log(i);
+    }
+  }
 
 
 
 /*Exercise #2: 
-Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].*/
+let varOne = "Launchcode";
+let varTwo = [1, 5, 'LC101', 'blue', 42];
+let reversed = "";
 
 
-Construct ``for`` loops to accomplish the following tasks:
+/*Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
+for (let i = 0; i < varTwo.length; i++) {
+  console.log(varTwo[i]);
+}
 
-
-
+for (let i = varOne.length - 1; i >= 0; i--) {
+  console.log(varOne[i]);
+}
 
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+
+  let allNumbers = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+  let evens = [];
+  let odds = [];
+
+for (let i = 0; i < allNumbers.length; i++) {
+  if (allNumbers[i] % 2 === 0) {
+    evens.push(allNumbers[i]);
+  } else {
+    odds.push(allNumbers[i]);
+  }
+}
+console.log(evens);
+console.log(odds);
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,53 @@
-//Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
+//Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.//
+const input = require('readline-sync');
+
+let startingFuelLevel;
+let numberOfAstronauts;
+let altitudeReached = 0;
+
 
 
 
 
 
 /*Exercise #4: Construct while loops to do the following:
-  a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
+a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
+startingFuelLevel = input.question('What is the starting fuel level?: ');
+startingFuelLevel = Number(startingFuelLevel);
 
-
-
-
-//b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
+while (startingFuelLevel <= 5000 || startingFuelLevel >= 30000) {
+    startingFuelLevel = input.question("Invalid input! Please enter a number between 5001 - 29999: ");
+    startingFuelLevel = Number(startingFuelLevel);
+}  
+  
+console.log("Perfect! Thank you for your input!");
   
   
-  
-  
-//c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
+//b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.//
+numberOfAstronauts = input.question('How many astronauts (<=7) are on board?: ');
+numberOfAstronauts = Number(numberOfAstronauts);
 
+while (numberOfAstronauts <= 0 || numberOfAstronauts > 7) {
+  numberOfAstronauts = input.question("Invalid input! Please enter a number between 1 - 7: ");
+  numberOfAstronauts = Number(numberOfAstronauts);
+}  
 
+console.log(`Thank you! Number of astronauts on this mission is ${numberOfAstronauts}`);
+
+//c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.//
+while (startingFuelLevel >= (100 * numberOfAstronauts)) {
+  startingFuelLevel = startingFuelLevel - (100 * numberOfAstronauts);
+  altitudeReached += 50;
+}
+
+console.log(`The shuttle gained an altitude of ${altitudeReached} km`);
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+if (altitudeReached >= 2000) {
+  console.log("Orbit achieved!");
+} else {
+  console.log("Failed to reach orbit.");
+}
