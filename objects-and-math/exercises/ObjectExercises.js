0a8717abdae4b49d3af14e26da4d71e1e9b81eb7
Bailey Johnson <bailey506@hotmail.com>
2024-06-06 15:10:13 -0500

    Objects Exercises

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,15 +2,74 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 1,
+   move: function() {return Math.floor(Math.random()*11)}
 };
+
+console.log(superChimpOne.move());
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function() {return Math.floor(Math.random()*11)}
 };
+
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function() {return Math.floor(Math.random()*11)}
+};
+
+let sweetBeags = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function() {return Math.floor(Math.random()*11)}
+};
+
+let tardybabe = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function() {return Math.floor(Math.random()*11)}
+};
+
+
+let crew = [superChimpOne, salamander, superChimpTwo, sweetBeags, tardybabe];
+
+
+function crewReports(animalAstronaut) {
+   return `${animalAstronaut.name} is a ${animalAstronaut.species}. They are ${animalAstronaut.age} years old and ${animalAstronaut.mass} kg. Their ID is ${animalAstronaut.astronautID}`;
+
+}
+
+
+function fitnessTest(candidates) {
+   let results = [], numSteps, turns;
+   for (let i = 0; i < candidates.length; i++){
+      numSteps = 0;
+      turns = 0;
+      while(numSteps < 20) {
+         numSteps += candidates[i].move();
+         turns++;
+      }
+      results.push(`${candidates[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+}
+
+console.log(crewReports(superChimpOne));
 
 
 // After you have created the other object literals, add the astronautID property to each one.
