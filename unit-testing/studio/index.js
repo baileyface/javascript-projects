3699de1e70f8692a58d8bb676382a15b73019225
Bailey Johnson <bailey506@hotmail.com>
2024-07-09 14:11:09 -0500

    Classes Exercises

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,34 @@
 
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: "100",
+    programsOffered: ['Web Development', 'Data Analysis', 'Liftoff'],
+    launchOutput: (number) => {
+        if (number % 2 === 0 && number % 3 === 0 && number % 5 === 0) {
+            return 'LaunchCode Rocks!';
+        else if (number % 2 === 0 && number % 3 === 0) {
+            return 'LaunchCode!';
+        else if (number % 3 === 0 &&& number % 5 === 0) {
+            return 'Code Rocks!';
+        else if (number % 2 === 0 && number % 5 === 0) {
+            return 'Launch Rocks! (CRASH!!!!)';
+        else if (number % 2 === 0) {
+            return 'Launch!';
+        else if (number % 3 === 0) {
+            return 'Code!';
+        else if (number % 5 === 0) {
+            return 'Rocks!';
+        } else {
+            return "Rutabagas! That doesn't work.";
+        }
+    }
+}
+        }
+        }
+        }
+    }
+  }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,36 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
-
   // Write your unit tests here!
   
+  test("should return 'nonprofit' for organization key", () => {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+  
+  test("should return 'Jeff' for executiveDirector key", () => {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("should return 100 for percentageCoolEmployees key", () => {
+    expect(launchcode.percentageCoolEmployees).toBe("100");
+  });
+
+  test("should correctly validate programsOffered array", () => {
+    expect(launchcode.programsOffered).toContain('Web Development');
+    expect(launchcode.programsOffered).toContain('Data Analysis');
+    expect(launchcode.programsOffered).toContain('Liftoff');
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+
+  test("launchOutput should return the correct strings based on divisibility", () => {
+    expect(launchcode.launchOutput(2)).toBe('Launch!');
+    expect(launchcode.launchOutput(3)).toBe('Code!');
+    expect(launchcode.launchOutput(5)).toBe('Rocks!');
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks!');
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+    expect(launchcode.launchOutput(7)).toBe("Rutabagas! That doesn't work.");
+  })
+
 });
