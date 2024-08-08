c306baa7bb676b3adca6c601fe6c09b3d6e5b224
Bailey Johnson <bailey506@hotmail.com>
2024-08-08 18:17:07 -0500

    Class 15 Exercises

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,20 @@
 //Code Your Solution Below
+
+document.getElementById('rocketForm').addEventListener('submit', function(event) {
+    let isValid = true;
+    const inputs = document.querySelectorAll('input[required], select[required]');
+
+    inputs.forEach(function(input) {
+        if(!input.value) {
+            isValid = false;
+            input.classList.add('error')
+        } else{
+            input.classList.remove('error');
+        }
+    });
+
+    if(!isValid) {
+        event.preventDefault();
+        alert('Please fill out all required fields.');
+    }
+});
